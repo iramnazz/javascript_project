d1b8d040d217a4d8d188c7fac121ebfd141bf6a7
iramnazz <iramnaz948@gmail.com>
2026-01-15 08:07:02 +0530

    Calculator Project

--- script.js
+++ script.js
@@ -526,15 +526,33 @@
 // calculator(10,15,add);
 // calculator(25,15,subtract);
 
-function calSquare (num,callback){
-    let square=num*num
-    callback(square);
-}
-function printSquare(result){
-    console.log("the square is:",result);
-}
-calSquare(10,printSquare);
-
+// function calSquare (num,callback){
+//     let square=num*num
+//     callback(square);
+// }
+// function printSquare(result){
+//     console.log("the square is:",result);
+// }
+// calSquare(10,printSquare);
+let string=" ";
+let buttons=document.querySelectorAll('.button');
+Array.from(buttons).forEach((button)=>{
+    button.addEventListener('click', (e)=>{
+        if(e.target.innerHTML =='='){
+            string = eval(string);
+             document.querySelector('input').value=string;
+        }
+        else if(e.target.innerHTML =='C'){
+            string = ""
+             document.querySelector('input').value=string;
+        }
+        else{
+        console.log(e.target)
+        string=string + e.target.innerHTML;
+        document.querySelector('input').value=string;
+        }
+    })
+})
 
 
 

c6dd63cb7ac6701b593f906aa46d9272e0e75989
iramnazz <iramnaz948@gmail.com>
2026-01-16 20:45:54 +0530

    DOM in JS

--- script.js
+++ script.js
@@ -553,7 +553,14 @@
 //         }
 //     })
 // })
-
+console.log("DOM")
+ document.body.childNodes[3].innerText= "Hellow World";
+ let heading=document.getElementById("heading")
+ console.dir(heading);
+ let headings= document.getElementsByClassName("heading");
+ console.dir(headings);
+let parahs= document.getElementsByTagName("p")
+console.dir(parahs);
 
 
 

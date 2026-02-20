01f202e297a2b72d42bff5ceee8b2e49e0709382
iramnazz <iramnaz948@gmail.com>
2026-02-20 09:21:49 +0530

    my portfolio

--- script.js
+++ script.js
@@ -888,5 +888,24 @@
 // .catch((err)=>{
    
 // })
-console.log("hello world");
-console.log("hello world");
+// console.log("one");
+// console.log("two");
+// setTimeout(()=>{
+//     console.log("'hello");
+// }, 4000);
+// console.log("three");
+// console.log("four")
+function getData(dataId,getNextData){
+    setTimeout(()=>{
+     console.log("data", dataId);
+     if (getNextData){
+     getNextData();
+     }
+    }, 2000);  
+} 
+getD ata(1,()=>{
+    getData(2)
+    });
+
+
+
